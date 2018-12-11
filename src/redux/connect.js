f872a642f196a9242a8798b147ee3bae2c5e6695
unknown <kianyin1992@gmail.com>
2018-12-12 00:14:01 +0800

    connect with subscription

--- src/redux/connect.js
+++ src/redux/connect.js
@@ -7,11 +7,30 @@ const connect = (WrappedComponent) => {
       store: PropTypes.object
     }
 
-    render () {
+    constructor () {
+      super()
+      this.state = { allProps: {} }
+    }
+
+    componentWillMount () {
+      const { store } = this.context
+      this._updateProps()
+      store.subscribe(() => this._updateProps())
+    }
+
+    _updateProps () {
       const { store } = this.context
       let stateProps = store.getState()
       stateProps.dispatch=store.dispatch
-      return <WrappedComponent {...stateProps}/>
+      this.setState({
+        allProps: { // 整合普通的 props 和从 state 生成的 props
+          ...stateProps,
+          ...this.props
+        }
+      })
+    }
+    render () {
+      return <WrappedComponent {...this.state.allProps}/>
     }
   }
 
